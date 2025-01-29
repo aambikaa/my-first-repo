040bc9123ff9b912da0dcfcf3fff60df3b117ac0
Ambika <shekhawatambika1999@gmail.com>
2025-01-29 14:02:51 +0530

    Implicit and Explicit wait

--- src/main/java/com/selenium/practice/LaunchBrowser.java
+++ src/main/java/com/selenium/practice/LaunchBrowser.java
@@ -1,9 +1,14 @@
 package com.selenium.practice;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchBrowser {
@@ -25,19 +30,22 @@ public class LaunchBrowser {
             // Maximize the browser window
             driver.manage().window().maximize();
 
-            // Find the search box using the name attribute
-            WebElement searchBox = driver.findElement(By.name("q"));
-            Thread.sleep(3000);
+            // Implicit wait 
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            // Find the search box using the name attribute (Explicit wait)
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
 
             // Type "Sachin" into the search box
             searchBox.sendKeys("Sachin");
-            Thread.sleep(3000);
 
             // Submit the search form
             searchBox.submit();
 
             // Wait for a few seconds to see the search results
-            Thread.sleep(3000); // Note: This is for demonstration purposes only
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // Note: This is for demonstration purposes only
+        
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
